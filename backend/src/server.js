7076b4eeb933288c1713125ae4d160c83ffa7717
FaheemAhamed <skbuddu786786@gmail.com>
2026-07-24 16:23:21 +0530

    feat: integrate MongoDB Atlas with Mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 import dotenv from "dotenv";
-import app from './app.js';
+import app from "./app.js";
+import connectDB from "./config/database.js";
 
-//Loading the enviornment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 500;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
 
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on http://localhost:${PORT}`);
+        });
 
+    } catch (error) {
+        console.error("Server startup failed:", error.message);
+    }
+};
 
+startServer();
